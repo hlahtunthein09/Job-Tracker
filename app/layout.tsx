bf22b24b70bb37ec0fa64f0bbfc91386e5f268f2
hlahtunthein09 <hlahtuntheinbatman@gmail.com>
2026-04-15 05:16:20 +0700

    fix deploy issues

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/navbar";
 import InactivityMonitor from "@/components/inactivity-monitor";
+import { Suspense } from "react";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,9 +31,11 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body suppressHydrationWarning={true} className="min-h-full flex flex-col">
-        <InactivityMonitor />
-        <Navbar />
-        {children}
+        <Suspense fallback={<div className="flex h-screen items-center justify-center">Loading...</div>}>
+          <InactivityMonitor />
+          <Navbar />
+          {children}
+        </Suspense>
       </body>
     </html>
   );
